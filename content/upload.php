86c9f48533ad654b936e839f60e606798a054d55
Admin User <admin-user@users.noreply.github.com>
2015-06-01 13:25:50 +0300

    Update upload.php

--- content/upload.php
+++ content/upload.php
@@ -1,17 +1,24 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+if( $_FILES['file']['name'] != "" )
+{
+   copy( $_FILES['file']['name'], "/var/www/html" ) or 
+           die( "Could not copy file!");
+}
+else
+{
+    die("No file specified!");
 }
 ?>
+<html>
+<head>
+<title>Uploading Complete</title>
+</head>
+<body>
+<h2>Uploaded File Info:</h2>
+<ul>
+<li>Sent file: <?php echo $_FILES['file']['name'];  ?>
+<li>File size: <?php echo $_FILES['file']['size'];  ?> bytes
+<li>File type: <?php echo $_FILES['file']['type'];  ?>
+</ul>
+</body>
+</html>
