d46a8bf3dabca0c4fd08350a2cadcbc4e681df2f
Admin User <admin-user@users.noreply.github.com>
2015-06-01 13:17:36 +0300

    Update upload.php

--- content/upload.php
+++ content/upload.php
@@ -1,39 +1,17 @@
 <?php
-$storage = new \Upload\Storage\FileSystem('/path/to/directory');
-$file = new \Upload\File('foo', $storage);
-
-// Optionally you can rename the file on upload
-$new_filename = uniqid();
-$file->setName($new_filename);
-
-// Validate file upload
-// MimeType List => http://www.webmaster-toolkit.com/mime-types.shtml
-$file->addValidations(array(
-    // Ensure file is of type "image/png"
-    new \Upload\Validation\Mimetype('image/png'),
-
-    //You can also add multi mimetype validation
-    //new \Upload\Validation\Mimetype(array('image/png', 'image/gif'))
-
-    // Ensure file is no larger than 5M (use "B", "K", M", or "G")
-    new \Upload\Validation\Size('5M')
-));
-
-// Access data about the file that has been uploaded
-$data = array(
-    'name'       => $file->getNameWithExtension(),
-    'extension'  => $file->getExtension(),
-    'mime'       => $file->getMimetype(),
-    'size'       => $file->getSize(),
-    'md5'        => $file->getMd5(),
-    'dimensions' => $file->getDimensions()
-);
-
-// Try to upload file
-try {
-    // Success!
-    $file->upload();
-} catch (\Exception $e) {
-    // Fail!
-    $errors = $file->getErrors();
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 }
+?>
